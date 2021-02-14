249f2daf71fd9ca9affcded476c36b98146b3339
= <=>
2021-02-14 13:29:18 +0530

    deploy

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-admin-site'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-admin-site/index.html'));
+res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log('server started..')
